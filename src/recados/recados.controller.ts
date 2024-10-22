d3c386a190159a5490d005f3254c9a5b61fad631
Dailton Bastos <dailtonbastos@gmail.com>
2024-10-22 09:35:28 -0300

    rota para atualizar (update) com metodos PATCH ou PUT

--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -5,8 +5,21 @@ import {
   HttpCode,
   HttpStatus,
   Param,
+  Patch,
   Post,
 } from '@nestjs/common';
+
+/* 
+  CRUD
+  Create -> POST -> Criar um recado
+  Read -> GET -> Ler todos os recados
+  Read -> GET -> Ler apenas um recado
+  Update -> PATCH / PUT -> Atualizar um recado
+  Delete -> DELETE -> Apagar um recado
+
+  PATCH é utilizado para atualizar dados de um recurso
+  PUT é utilizado para atualizar um recurso inteiro
+*/
 
 @Controller('recados')
 export class RecadosController {
@@ -27,5 +40,13 @@ export class RecadosController {
   @Post()
   create(@Body() body: any) {
     return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body: any) {
+    return {
+      id,
+      ...body,
+    };
   }
 }
