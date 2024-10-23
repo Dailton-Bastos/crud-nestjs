c50d6156586145570002efb9f427d401dacef8ed
Dailton Bastos <dailtonbastos@gmail.com>
2024-10-23 09:39:06 -0300

    configurando o TypeOrmModule com PostgreSQL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecadosModule } from 'src/recados/recados.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [RecadosModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'ep-quiet-wave-a5dlqzvv.us-east-2.aws.neon.tech',
+      port: 5432,
+      username: 'uberdb_owner',
+      password: 'dGYN21BhCWLp',
+      database: 'uberdb',
+      autoLoadEntities: true, // Carrega entidades sem precisar especifica-las
+      synchronize: true, // Sincroniza com o BD. Não usar em prod
+      ssl: true,
+    }),
+    RecadosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
