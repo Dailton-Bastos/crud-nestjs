deb7537748d27ea0f650e5adcbc339bed510c8b4
Dailton Bastos <dailtonbastos@gmail.com>
2024-10-25 22:27:32 -0300

    injecao de dependencia em interceptors (Injectable)

--- src/common/interceptors/change-data.interceptor.ts
+++ src/common/interceptors/change-data.interceptor.ts
@@ -1,6 +1,12 @@
-import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
 import { map, Observable } from 'rxjs';
 
+@Injectable()
 export class ChangeDataInterceptor implements NestInterceptor {
   intercept(_context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
