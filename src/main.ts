36d7d46ae525c1407ee88c8a716265a806c45c36
Dailton Bastos <dailtonbastos@gmail.com>
2025-10-25 17:53:01 -0300

    configurando os testes end-to-end (E2E)

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module'; // Módulo raiz
-import { ValidationPipe } from '@nestjs/common';
-import { ParseIntIdPipe } from './common/pipes/parse-int-id.pipe';
+import appConfig from './app/config/app.config';
 // import { IsAdminGuard } from './common/guards/is-admin.guard';
 // import { MyExceptionFilter } from './common/filters/my-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true, // remove chaves que não estão no DTO
-      forbidNonWhitelisted: true, // levantar erro quando a chave não existir
-      transform: false, // tenta transformar os tipos de dados de params e dtos
-    }),
-    new ParseIntIdPipe(),
-  );
+  appConfig(app);
 
   // app.useGlobalFilters(new MyExceptionFilter());
   // app.useGlobalGuards(new IsAdminGuard())
---
+++ test/pessoas.e2e-spec.ts
@@ -0,0 +1,56 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import globalConfig from 'src/global-config/global.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { resolve } from 'node:path';
+import { RecadosModule } from 'src/recados/recados.module';
+import { PessoasModule } from 'src/pessoas/pessoas.module';
+import { GlobalConfigModule } from 'src/global-config/global-config.module';
+import { AuthModule } from 'src/auth/auth.module';
+import appConfig from 'src/app/config/app.config';
+// import * as request from 'supertest';
+
+describe('PessoasController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forFeature(globalConfig),
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: 'localhost',
+          port: 5432,
+          username: 'postgres',
+          password: 'postgres',
+          database: 'testing',
+          autoLoadEntities: true,
+          synchronize: true,
+          dropSchema: true,
+        }),
+        ServeStaticModule.forRoot({
+          rootPath: resolve(__dirname, '..', '..', 'pictures'),
+          serveRoot: '/pictures',
+        }),
+        RecadosModule,
+        PessoasModule,
+        GlobalConfigModule,
+        AuthModule,
+      ],
+    }).compile();
+
+    app = module.createNestApplication();
+
+    appConfig(app);
+
+    await app.init();
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
+
+  it('/ (GET)', () => {});
+});
