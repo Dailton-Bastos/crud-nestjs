ff6602a6be9b93bc3ee7e5ddcdcd89f62c06ab68
Dailton Bastos <dailtonbastos@gmail.com>
2025-10-25 22:26:13 -0300

    mais exemplos de testes E2E em codigo para a rota /pessoas

--- test/pessoas.e2e-spec.ts
+++ test/pessoas.e2e-spec.ts
@@ -166,6 +166,41 @@ describe('PessoasController (e2e)', () => {
     });
   });
 
+  describe('GET /pessoas', () => {
+    it('deve retornar todas as pessoas', async () => {
+      const dto = {
+        email: 'teste@teste.com',
+        nome: 'Teste',
+        password: '123456',
+      };
+
+      await request(app.getHttpServer())
+        .post('/pessoas')
+        .send(dto)
+        .expect(HttpStatus.CREATED);
+
+      const acessToken = await login(app, {
+        email: dto.email,
+        password: dto.password,
+      });
+
+      const response = await request(app.getHttpServer())
+        .get('/pessoas')
+        .set('Authorization', `Bearer ${acessToken}`)
+        .expect(HttpStatus.OK);
+
+      expect(response.body).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            id: expect.any(Number),
+            email: dto.email,
+            nome: dto.nome,
+          }),
+        ]),
+      );
+    });
+  });
+
   describe('GET /pessoas/:id', () => {
     it('deve retornar UNAUTHORIZED se não estiver autenticado', async () => {
       const pessoaResponse = await request(app.getHttpServer())
@@ -227,6 +262,150 @@ describe('PessoasController (e2e)', () => {
       // });
 
       expect(response.body).toEqual(pessoaResponse.body);
+    });
+
+    it('deve retornar NOT_FOUND se a pessoa não for encontrada', async () => {
+      await request(app.getHttpServer())
+        .post('/pessoas')
+        .send({
+          email: 'teste@teste.com',
+          nome: 'Teste',
+          password: '123456',
+        })
+        .expect(HttpStatus.CREATED);
+
+      const acessToken = await login(app, {
+        email: 'teste@teste.com',
+        password: '123456',
+      });
+
+      const response = await request(app.getHttpServer())
+        .get(`/pessoas/999`)
+        .set('Authorization', `Bearer ${acessToken}`);
+
+      expect(response.status).toBe(HttpStatus.NOT_FOUND);
+
+      expect(response.body).toEqual({
+        error: 'Not Found',
+        statusCode: HttpStatus.NOT_FOUND,
+        message: 'Pessoa não encontrada',
+      });
+    });
+  });
+
+  describe('PATCH /pessoas/:id', () => {
+    it('deve atualizar uma pessoa com sucesso', async () => {
+      const dto = {
+        email: 'teste@teste.com',
+        nome: 'Teste',
+        password: '123456',
+      };
+
+      const pessoaResponse = await request(app.getHttpServer())
+        .post('/pessoas')
+        .send(dto)
+        .expect(HttpStatus.CREATED);
+
+      const acessToken = await login(app, {
+        email: dto.email,
+        password: dto.password,
+      });
+
+      const response = await request(app.getHttpServer())
+        .patch(`/pessoas/${pessoaResponse.body.id}`)
+        .set('Authorization', `Bearer ${acessToken}`)
+        .send({ nome: 'Teste Atualizado' })
+        .expect(HttpStatus.OK);
+
+      expect(response.body).toEqual(
+        expect.objectContaining({
+          id: pessoaResponse.body.id,
+          nome: 'Teste Atualizado',
+        }),
+      );
+    });
+
+    it('deve retornar NOT_FOUND se a pessoa não for encontrada', async () => {
+      await request(app.getHttpServer())
+        .post('/pessoas')
+        .send({
+          email: 'teste@teste.com',
+          nome: 'Teste',
+          password: '123456',
+        })
+        .expect(HttpStatus.CREATED);
+
+      const acessToken = await login(app, {
+        email: 'teste@teste.com',
+        password: '123456',
+      });
+
+      const response = await request(app.getHttpServer())
+        .patch(`/pessoas/999`)
+        .set('Authorization', `Bearer ${acessToken}`);
+
+      expect(response.status).toBe(HttpStatus.NOT_FOUND);
+
+      expect(response.body).toEqual({
+        error: 'Not Found',
+        statusCode: HttpStatus.NOT_FOUND,
+        message: 'Pessoa não encontrada',
+      });
+    });
+  });
+
+  describe('DELETE /pessoas/:id', () => {
+    it('deve deletar uma pessoa com sucesso', async () => {
+      const dto = {
+        email: 'teste@teste.com',
+        nome: 'Teste',
+        password: '123456',
+      };
+
+      const pessoaResponse = await request(app.getHttpServer())
+        .post('/pessoas')
+        .send(dto)
+        .expect(HttpStatus.CREATED);
+
+      const acessToken = await login(app, {
+        email: dto.email,
+        password: dto.password,
+      });
+
+      const response = await request(app.getHttpServer())
+        .delete(`/pessoas/${pessoaResponse.body.id}`)
+        .set('Authorization', `Bearer ${acessToken}`)
+        .expect(HttpStatus.OK);
+
+      expect(response.body.email).toEqual(pessoaResponse.body.email);
+    });
+
+    it('deve retornar NOT_FOUND se a pessoa não for encontrada', async () => {
+      await request(app.getHttpServer())
+        .post('/pessoas')
+        .send({
+          email: 'teste@teste.com',
+          nome: 'Teste',
+          password: '123456',
+        })
+        .expect(HttpStatus.CREATED);
+
+      const acessToken = await login(app, {
+        email: 'teste@teste.com',
+        password: '123456',
+      });
+
+      const response = await request(app.getHttpServer())
+        .delete(`/pessoas/999`)
+        .set('Authorization', `Bearer ${acessToken}`);
+
+      expect(response.status).toBe(HttpStatus.NOT_FOUND);
+
+      expect(response.body).toEqual({
+        error: 'Not Found',
+        statusCode: HttpStatus.NOT_FOUND,
+        message: 'Pessoa não encontrada',
+      });
     });
   });
 });
